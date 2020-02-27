294d7e25b2c8cc2b9d94cba008393b0edb76a5ac
FionaViola <aditi.nadig10@gmail.com>
2020-02-27 02:41:03 -0500

    Updated with react components untill index.js

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import CurrencyRow from './CurrencyRow'
+
+const BASE_URL = "https://api.exchangeratesapi.io/latest"
+
+function App() {
+
+  const [currencyOptions, setCurrencyOptions] = useState([])
+
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => {
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+      })
+  }, [])
+  return (
+    <>
+    <h1>Currency Converter</h1>
+    <CurrencyRow
+      currencyOptions={currencyOptions}
+    />
+    
+    <div className="equals"> = </div>
+    <CurrencyRow/>
+    </>
+  );
+}
+
+export default App;
